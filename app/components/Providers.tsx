6245503b5fc8f161c15d6d2fd833e6f09682cdb3
Omar Islam <Moraeslam1@gmail.com>
2024-03-04 11:33:53 +0200

    .

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,12 +1,22 @@
-"use client"
-import React, { ReactNode } from 'react'
-import { CartProvider as USCProvider } from 'use-shopping-cart'
-const CartProvider = ({ children }: { children: ReactNode }) => {
+"use client";
+
+import { ReactNode } from "react";
+import { CartProvider as USCProvider } from "use-shopping-cart";
+
+export default function CartProvider({ children }: { children: ReactNode }) {
     return (
-        <USCProvider language='en-US' cartMode='client-only' stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string} successUrl='http://localhost:3000/stripe/success' cancelUrl='http://localhost:3000/stripe/error' currency='USD' billingAddressCollection={false} shouldPersist>
+        <USCProvider
+            mode="payment"
+            cartMode="client-only"
+            stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
+            successUrl="https://https://next-eccomerce-omarzaeda.vercel.app//stripe/success"
+            cancelUrl="https://https://next-eccomerce-omarzaeda.vercel.app/stripe/error"
+            currency="USD"
+            billingAddressCollection={false}
+            shouldPersist={true}
+            language="en-US"
+        >
             {children}
         </USCProvider>
-    )
+    );
 }
-
-export default CartProvider
